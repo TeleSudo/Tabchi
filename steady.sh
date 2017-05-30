a2ca5f38b06f0146169d52ef33941957d32f8e1d
Abolfazl Fallah <fallahabolfazl2@yahoo.com>
2017-05-30 17:16:13 +0430

    Update steady.sh

--- steady.sh
+++ steady.sh
@@ -30,7 +30,7 @@ NONVOLUNTARYCHECK=0
 VOLUNTARY=1
 VOLUNTARYCHECK=0
 I=1
-BOT=tabchi  # You can put here other bots. Also you can change it to run more than one bot in the same server.
+BOT=TTabchi  # You can put here other bots. Also you can change it to run more than one bot in the same server.
 RELOADTIME=10  # Time between checking cpu calls of the cli process. Set the value high if your bot does not receive lots of messages.
 
 
